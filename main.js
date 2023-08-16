677e8f2e380eb79282c12955d10fe3491684aea6
rmanikandan2001 <rmanikandan6867@gmail.com>
2023-08-15 23:19:48 -0700

    add

---
+++ main.js
@@ -0,0 +1,22 @@
+
+
+	let A = parseInt( prompt("enter marks"));
+
+	let B =parseInt( prompt("enter marks"));
+
+	let C = parseInt( prompt("enter marks"));
+
+	let D =parseInt( prompt("enter marks"));
+
+	let E= parseInt(prompt("enter marks"));
+
+	
+
+	let total=(A+B+C+D+E);
+		
+		console.log(total);
+		
+		let avg = total/5;
+		
+		console.log(avg);
+		
